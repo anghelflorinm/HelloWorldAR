8f73f2f219a0e755417b627713248fb7e77db724
Florin ANGHEL <anghel.florin.m@gmail.com>
2020-10-07 22:30:22 +0300

    add codebase

---
+++ HelloWorld/Assets/ImageBehavious.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+//using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ImageBehavious : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform cameraTransform;
+    public Animator animator;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform transform = GetComponent<Transform>();
+        float dist = Vector3.Distance(transform.position, cameraTransform.position);
+        UnityEngine.Debug.Log(dist);
+        if (dist < 0.5)
+        {
+            animator.SetTrigger("AnimationTrigger");
+        }
+        else
+        {
+            animator.ResetTrigger("AnimationTrigger");
+        }
+    }
+}
